9e65deebaa4fc2a2f4dfaf6a2c6c916c0e5f6628
Faik Turan <turanfaik@hotmail.com>
2022-09-26 19:43:55 +0300

    client side changed

---
+++ ecommerce-app/client/src/api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+axios.interceptors.request.use(
+    function(config){
+        const { origin } = new URL(config.url);
+
+        const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
+        const token = localStorage.getItem("access-token");
+
+        if(allowedOrigins.includes(origin)){
+            config.headers.authorization = token;
+        }
+
+        return config;
+    },
+    function(error){
+        return Promise.reject(error);
+    }
+);
+
+export const fetchProductList = async ({ pageParam = 1 }) => {
+    const { data } = 
+    await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
+    return data;
+}
+
+export const fetchProduct = async (id) => {
+    const { data } = 
+    await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product${id}`);
+    return data;
+}
+
+export const fetchRegister = async (input) => {
+    const { data } = 
+    await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`, input);
+    return data;
+}
