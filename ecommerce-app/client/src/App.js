78d1329e094761920aed88f6a921608ccc9cf9a1
Faik Turan <turanfaik@hotmail.com>
2022-10-02 20:21:44 +0300

    react client side done

--- ecommerce-app/client/src/App.js
+++ ecommerce-app/client/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import ProtectedRoute from "./pages/ProtectedRoutes";
+
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
+import Profile from "./pages/Profile";
+import Basket from "./pages/Basket";
+import Admin from "./pages/Admin";
+import Error404 from "./pages/Error404";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+    <div>
+      <Navbar />
+
+      <div id="content">
+      <Switch>
+        <Route path="/" exact component={Products} />
+        <Route path="/product/:product_id" component={ProductDetail} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/basket" component={Basket} />
+        <ProtectedRoute path="/profile" component={Profile} />
+        <ProtectedRoute path="/admin" component={Admin} admin={true} />
+        <ProtectedRoute path="*" component={Error404} />
+      </Switch>
+      </div>
     </div>
+   </Router>
   );
 }
 
--- ecommerce-app/client/src/index.js
+++ ecommerce-app/client/src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./reset.css";
+import "antd/dist/antd.css";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+// contexts
+import { AuthProvider } from "./contexts/AuthContext";
+import { BasketProvider } from "./contexts/BasketContext";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+  <ChakraProvider>
+    <AuthProvider>
+      <BasketProvider>
+        <App />
+      </BasketProvider>
+    </AuthProvider>
+  </ChakraProvider>
+
+  <ReactQueryDevtools initialIsOpen={false}  />
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
