dd3953642084d85257c9cc012d80356d64d1ab6f
Faik Turan <turanfaik@hotmail.com>
2022-10-02 19:47:19 +0300

    client side continue

--- ecommerce-app/client/src/pages/Admin/Orders/index.js
+++ ecommerce-app/client/src/pages/Admin/Orders/index.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
-function index() {
+import {
+Table,
+Thead,
+Tbody,
+Tr,
+Th,
+Td,
+TableCaption,
+Text,
+} from "@chakra-ui/react"
+import { useQuery } from "react-query";
+import { fetchOrders } from "../../../api";
+
+
+function Orders() {
+    const {isLoading, isError, data, error } = useQuery(
+        "admin:orders",
+        fetchOrders
+    );
+
+    if(isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if(isError){
+        return <div>Error {error.message}</div>
+    }
+
   return (
-    <div>index</div>
+    <div>
+        <Text fontSize="2xl" p={5}>
+            Orders
+        </Text>
+
+        <Table variant="simple">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+                <Tr>
+                    <Th>User</Th>
+                    <Th>Address</Th>
+                    <Th isNumeric>Items</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {data.map((item) => {
+                    <Tr key={item._id}>
+                        <Td>{item.user.email}</Td>
+                        <Td>{item.address}</Td>
+                        <Td isNumeric>{item.items.length}</Td>
+                    </Tr>
+                })}
+            </Tbody>
+        </Table>
+    </div>
   )
 }
 
-export default index
+export default Orders;
---
+++ ecommerce-app/client/src/pages/Error404/index.js
@@ -0,0 +1,20 @@
+import {
+Alert,
+AlertIcon,
+AlertTitle,
+AlertDescription,
+} from "@chakra-ui/react";
+
+function Error404() {
+  return (
+    <div>
+        <Alert status="error">
+            <AlertIcon />
+            <AlertTitle mr={2}>Error404</AlertTitle>
+            <AlertDescription>This page not found.</AlertDescription>
+        </Alert>
+    </div>
+  )
+}
+
+export default Error404;
